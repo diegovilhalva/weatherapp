9522c87b0dc02a85539da8f27c08714baeae0993
Diego Vilhalva <89986274+diegovilhalva@users.noreply.github.com>
2024-10-03 13:45:39 -0300

    Update module.js

--- assets/js/module.js
+++ assets/js/module.js
@@ -38,7 +38,7 @@ export const getTime = function(timeUnix, timezone) {
     const date = new Date((timeUnix + timezone) * 1000);
     const hours = date.getUTCHours();
     const minutes = date.getUTCMinutes();
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}h`;
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
 }
 
 
